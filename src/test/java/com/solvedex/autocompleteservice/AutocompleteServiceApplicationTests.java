51edb3b60ba644407947da2377da2162fb38ec19
Jhonatan Espana <jersi2695@hotmail.com>
2023-07-27 23:11:33 -0500

    Added the skeleton of the project

---
+++ src/test/java/com/solvedex/autocompleteservice/AutocompleteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solvedex.autocompleteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocompleteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
